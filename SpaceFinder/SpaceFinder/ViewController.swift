3ecadc04589374a59047c2303c2d9b82a77d6917
Puneet Bagga <pbagga@rsgc.on.ca>
2019-02-16 14:45:43 -0500

    created app

---
+++ SpaceFinder/SpaceFinder/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SpaceFinder
+//
+//  Created by Puneet Bagga on 2019-02-16.
+//  Copyright © 2019 SpaceEvaders. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
